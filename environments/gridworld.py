edb3e5d8cb2df1a6eef533effe3cf8bb7027b292
sourav chakraborty <souravchk@souravs-MacBook-Pro.local>
2020-04-21 23:01:39 -0600

    fixed comparator

--- environments/gridworld.py
+++ environments/gridworld.py
@@ -4,23 +4,36 @@ class State:
         self.x = x
         self.y = y
 
+    def __hash__(self):
+        return int(str(hash(self.x)) + str(hash(self.y)))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return "({}, {})".format(self.x, self.y)
+
 
 class GridWorld:
-    def __init__(self, rows=4, cols=12, transitions=None, rewards=None):
+    def __init__(self, rows=4, cols=12):
         self.rows = rows
         self.cols = cols
-        self.transitions = transitions
-        self.rewards = rewards
+        self.transitions = {}
+        self.rewards = {}
         self.actions = {}
         self.state_space = []
         self.next_states = {}
         self.__fill_state_space()
         self.__fill_next_states_and_actions()
-        self.__fill_transitions()
         self.__fill_rewards()
+        self.__fill_transitions()
 
     def display(self):
-        print("Dimensions: ({} x {})\n".format(self.rows, self.cols))
+        print("Grid Dimensions: ({} x {})\n".format(self.rows, self.cols))
+        print("\n*States Space*: ", self.state_space)
+        print("\n\n*Actions*: ", self.actions)
+        print("\n\n*Rewards*: ", self.rewards)
+        print("\n\n*Transitions*: ", self.transitions)
 
     def __fill_state_space(self):
         for r in range(1, self.rows):
@@ -28,23 +41,46 @@ class GridWorld:
                 self.state_space.append(State(r, c))
 
     def __fill_transitions(self):
-        pass
+        for state in self.state_space:
+            nx, ny = state.x - 1, state.y
+            ex, ey = state.x, state.y + 1
+            wx, wy = state.x, state.y - 1
+            sx, sy = state.x + 1, state.y
+            if self.__is_valid(nx, ny):
+                self.transitions[1, state, 'N', State(nx, ny)] = 0.7
+                self.transitions[1, state, 'E', State(nx, ny)] = 0.1
+                self.transitions[1, state, 'W', State(nx, ny)] = 0.1
+                self.transitions[1, state, 'S', State(nx, ny)] = 0.1
+            if self.__is_valid(ex, ey):
+                self.transitions[1, state, 'N', State(ex, ey)] = 0.1
+                self.transitions[1, state, 'E', State(ex, ey)] = 0.7
+                self.transitions[1, state, 'W', State(ex, ey)] = 0.1
+                self.transitions[1, state, 'S', State(ex, ey)] = 0.1
+            if self.__is_valid(wx, wy):
+                self.transitions[1, state, 'N', State(wx, wy)] = 0.1
+                self.transitions[1, state, 'E', State(wx, wy)] = 0.1
+                self.transitions[1, state, 'W', State(wx, wy)] = 0.7
+                self.transitions[1, state, 'S', State(wx, wy)] = 0.1
+            if self.__is_valid(sx, sy):
+                self.transitions[1, state, 'N', State(sx, sy)] = 0.1
+                self.transitions[1, state, 'E', State(sx, sy)] = 0.1
+                self.transitions[1, state, 'W', State(sx, sy)] = 0.1
+                self.transitions[1, state, 'S', State(sx, sy)] = 0.7
 
     def __fill_rewards(self):
-        if self.rewards is None:
-            for state in self.state_space:
-                for action in self.actions.get(1, state):
-                    triplet = 1, state, action
-                    if state.x == 4 and state.y == 1 and action == 'E':
-                        self.rewards[triplet] = -100
-                    elif state.x == 4 and state.y == 12 and action == 'W':
-                        self.rewards[triplet] = -100
-                    elif state.x == 3 and state.y == 12 and action == 'S':
-                        self.rewards[triplet] = 100
-                    elif state.x == 3 and 2 <= state.y <= 11 and action == 'S':
-                        self.rewards[triplet] = -100
-                    else:
-                        self.rewards[triplet] = -1
+        for state in self.state_space:
+            for action in self.actions[1, state]:
+                triplet = 1, state, action
+                if state.x == 4 and state.y == 1 and action == 'E':
+                    self.rewards[triplet] = -100
+                elif state.x == 4 and state.y == 12 and action == 'W':
+                    self.rewards[triplet] = -100
+                elif state.x == 3 and state.y == 12 and action == 'S':
+                    self.rewards[triplet] = 100
+                elif state.x == 3 and 2 <= state.y <= 11 and action == 'S':
+                    self.rewards[triplet] = -100
+                else:
+                    self.rewards[triplet] = -1
 
     def __is_valid(self, x, y):
         return 1 <= x <= self.rows and 1 <= y <= self.cols
